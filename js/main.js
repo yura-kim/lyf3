371959b665505360e257e2a4012c00e44b10cf92
SanToki <yuraxkim@gmail.com>
2015-07-11 22:38:10 -0400

    Event logging; simple job functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var events = document.getElementById("events");
+
+function logEvent(string) {
+	var newEvent = document.createElement("p");
+	newEvent.innerHTML = string;
+	events.insertBefore(newEvent, events.childNodes[0]);
+}
+
 function MyCtor(element, data) {
     this.data = data;
     this.element = element;
@@ -19,20 +27,22 @@ MyCtor.prototype.change = function (value) {
 MyCtor.prototype.getValue = function () {
     return parseInt(this.element.innerHTML, 10);
 };
-
-var energy = new MyCtor(document.getElementById("energy"), 0);
-var age = new MyCtor(document.getElementById("age"), 0);
-ENERGY_INREASE_RATE = 1,
-ENERGY_INCREASE_INTERVAL = 100;
-
-setInterval(function () {
-    energy.change(energy.getValue() + ENERGY_INREASE_RATE);
-}, ENERGY_INCREASE_INTERVAL);
-
+// Stats
+var age = new MyCtor(document.getElementById("age"), 0),
+	job = new MyCtor(document.getElementById("job"), 0),
+	money = new MyCtor(document.getElementById("money"), 0),
+	energy = new MyCtor(document.getElementById("energy"), 0),
+	ENERGY_INREASE_RATE = 1,
+	ENERGY_INCREASE_INTERVAL = 1000;
+// Age increment
 setInterval(function () {
     age.change(age.getValue() + 1);
 }, 10000);
-
+// Energy Increment
+setInterval(function () {
+    energy.change(energy.getValue() + ENERGY_INREASE_RATE);
+}, ENERGY_INCREASE_INTERVAL);
+// Theme change
 var isDarkMode = false;
 $('button#theme').click(function () {
     if (isDarkMode) {
@@ -41,6 +51,40 @@ $('button#theme').click(function () {
     } else {
         $('link[href="css/lightstyle.css"]').attr('href','css/darkstyle.css');
         this.innerHTML = "Light Theme";
+    }
+    isDarkMode = !isDarkMode;
+});
+
+/* WORK */
+// Variables
+var WORK_COST = 2,
+	WORK_PAY = 2,
+	work_pay = new MyCtor(document.getElementById("work_pay"), 0),
+	work_cost = new MyCtor(document.getElementById("work_cost"), 0);
+// Work button
+$('button#work').click(function () {
+    if (energy.getValue() >= WORK_COST) {
+        energy.change(energy.getValue() - WORK_COST);
+        money.change(money.getValue() + WORK_PAY);
+        var message = ""
+        message = message.concat("You spent ", WORK_COST, " energy and made $", WORK_PAY, ".");
+        logEvent(message);
+    } else {
+    	logEvent("You don't have enough energy to work.")
+    }
+    isDarkMode = !isDarkMode;
+});
+// Promotion button
+$('button#promotion').click(function () {
+    if (job.element.innerHTML == "Dish Washer") {
+        job.change("Waiter");
+        WORK_PAY = 5;
+        work_pay.change(5);
+        WORK_COST = 2;
+        work_cost.change(2);
+        logEvent("You got a promotion!");
+    } else {
+        logEvent("You did not get a promotion...");
     }
     isDarkMode = !isDarkMode;
 });
