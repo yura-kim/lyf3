aaf2bbf8ce46b57595e581fdadbb5fe0714c13ae
SanToki <yuraxkim@gmail.com>
2015-07-11 20:57:01 -0400

    Button text change

--- js/main.js
+++ js/main.js
@@ -20,20 +20,23 @@ MyCtor.prototype.getValue = function () {
     return parseInt(this.element.value, 10);
 };
 
-var energy = new MyCtor(document.getElementById("energy"), 0);
-var ENERGY_INREASE_RATE = 1;
-var ENERGY_INCREASE_INTERVAL = 100;
+var energy = new MyCtor(document.getElementById("energy"), 0),
+	ENERGY_INREASE_RATE = 1,
+	ENERGY_INCREASE_INTERVAL = 100;
 
 setInterval(function () {
     energy.change(energy.getValue() + ENERGY_INREASE_RATE);
 }, ENERGY_INCREASE_INTERVAL);
 
 var isDarkMode = false;
+
 $('button#theme').click(function (){
     if (isDarkMode) {
         $('link[href="css/darkstyle.css"]').attr('href','css/lightstyle.css');
+        this.value = "Light";
     } else {
         $('link[href="css/lightstyle.css"]').attr('href','css/darkstyle.css');
+        this.value = "Dark";
     }
     isDarkMode = !isDarkMode;
 });
