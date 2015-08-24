78c26ca26f0899bac1c50fa48ecea4039b3401ed
SanToki <yuraxkim@gmail.com>
2015-08-23 20:48:10 -0400

    Cookies, save, auto-save, reset
    Cookies added for theme and stats
    Save and reset buttons work
    Auto-save setup every 3- seconds

--- js/main.js
+++ js/main.js
@@ -21,6 +21,48 @@ else{
 
 
 
+/* READ COOKIES */
+// First load ?
+if(localStorage.length == 0) {
+    localStorage.theme = "light";
+    defaultStats();
+}
+// Stats
+loadStats();
+// Theme
+if (localStorage.theme == "dark") {
+    $('link[href="css/lightstyle.css"]').attr('href','css/darkstyle.css');
+}
+
+function defaultStats() {
+    localStorage.age = Number(15);
+    localStorage.energy = Number(0);
+    localStorage.intelligence = Number(0);
+    localStorage.appearance = Number(0);
+    localStorage.job = "None";
+    localStorage.money = Number(0);
+}
+
+function saveStats() {
+    localStorage.age = Number(document.getElementById("age").innerHTML);
+    localStorage.energy = Number(document.getElementById("energy").innerHTML);
+    localStorage.intelligence = Number(document.getElementById("intelligence").innerHTML);
+    localStorage.appearance = Number(document.getElementById("appearance").innerHTML);
+    localStorage.job = document.getElementById("job").innerHTML;
+    localStorage.money = Number(document.getElementById("money").innerHTML);
+}
+
+function loadStats() {
+    document.getElementById("age").innerHTML = Number(localStorage.age);
+    document.getElementById("energy").innerHTML = Number(localStorage.energy);
+    document.getElementById("intelligence").innerHTML = Number(localStorage.intelligence);
+    document.getElementById("appearance").innerHTML = Number(localStorage.appearance);
+    document.getElementById("job").innerHTML = localStorage.job;
+    document.getElementById("money").innerHTML = Number(localStorage.money);
+}
+
+
+
 /* TABS */
 $(document).ready(function() {
     $('.tabs .tab-links a').on('click', function(e)  {
@@ -88,25 +130,47 @@ var age = new MyCtor(document.getElementById("age"), 0),
 	job = new MyCtor(document.getElementById("job"), 0),
 	money = new MyCtor(document.getElementById("money"), 0),
 	energy = new MyCtor(document.getElementById("energy"), 0),
+    AGE_INTERVAL = 30000,
 	ENERGY_INREASE_RATE = 1,
-	ENERGY_INCREASE_INTERVAL = 1000;
+	ENERGY_INCREASE_INTERVAL = 1000,
+    SAVE_INTERVAL = 10000;
 // Age increment
 setInterval(function () {
     age.change(age.getValue() + 1);
-}, 30000);
+}, AGE_INTERVAL);
 // Energy Increment
 setInterval(function () {
     energy.change(energy.getValue() + ENERGY_INREASE_RATE);
 }, ENERGY_INCREASE_INTERVAL);
-// Theme change
-var isDarkMode = false;
+// Save Increment
+setInterval(function() {
+    console.log("Auto-save");
+    saveStats();
+}, SAVE_INTERVAL);
+
+
+
+/* BUTTONS */
+// Save button
+$('button#save').click(function() {
+    saveStats();
+})
+// Reset button
+$('button#reset').click(function() {
+    if(confirm("Reset all game stats and achievements?")) {
+        defaultStats();
+        loadStats();
+    }
+})
+// Theme button
 $('button#theme').click(function () {
-    if (isDarkMode) {
+    if (localStorage.theme == "dark") {
         $('link[href="css/darkstyle.css"]').attr('href','css/lightstyle.css');
         this.innerHTML = "Dark Theme";
+        localStorage.theme = "light";
     } else {
         $('link[href="css/lightstyle.css"]').attr('href','css/darkstyle.css');
         this.innerHTML = "Light Theme";
+        localStorage.theme = "dark";
     }
-    isDarkMode = !isDarkMode;
 });
